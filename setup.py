2125eccb11229d4d218dae6d80854a3f341ef8b6
Alex Guckenberger <a8l5wzz@mmm.com>
2020-12-16 14:31:23 -0600

    inc version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import subprocess
 from setuptools import setup, find_packages
 
 NAME = 'requirement-walker'
-VERSION = '0.0.5'
+VERSION = '0.0.6'
 AUTHOR = 'Alex Guckenberger'
 AUTHOR_EMAIL = 'aguckenberger@mmm.com'
 DESCRIPTION = 'Walk through requirements and comments in requirements.txt files.'
